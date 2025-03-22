0d9dee9e754c54dd7a1241bdca70944fde21463f
vicky111999 <vigneshsaravanan261@gmail.com>
2025-03-21 13:44:02 -1200

    array method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,235 @@
-function App()
+/*function Vicky()
+{
+  var arr=[11,11,11];
+  var json={name:"vicky",mark:20};
+  var n1=11;
+  var b=true;
+  return(
+    <>
+    {typeof(arr)}<br></br>
+    {typeof(json)}<br></br>
+    {typeof(n1)}<br></br>
+    {typeof(b)}
+    <h1>Find Array</h1>
+    {Array.isArray(arr) && "is array"}<br></br>
+    {(Array.isArray(json) && "is array") || "is not array"}
+    </>
+  )
+}
+export default Vicky
+/*function Vicky()
+{
+  var arr=[-10,-10,10,20,20]
+  var sum=0;
+  var big=[0];
+  var even=0;
+  var neg=0;
+  return(
+    <>
+  <h1 style={{disply:"none"}}>{arr.map((item)=>sum=sum+item)}</h1>
+    <h1>sum of array:{sum}</h1>
+   <h2>biggest value</h2>
+    {arr.map((item)=>big=big<item)}
+    <h2>big:{big}</h2>
+    {arr.map((item)=>big<item && <>{big=item}</>)}
+    <h1>{big}</h1>
+    {arr.map((item)=>big<item && <>{big=item}</>)}
+<h1>count even no</h1>
+{arr.map((item)=>item%2===0 && <>{even+=1}</>)}
+<h1>even:{even}</h1>
+{arr.map((item)=>item<0 && <>{neg+=1}</>)}
+<h1>even:{neg}</h1>
+
+    </>
+  )
+}
+export default Vicky
+/*function Vicky()
+{
+  var arr=[11,22,33]
+  var sum=0;
+  for(i=0;i<=arr.length;i++)
+  {
+    sum=sum+arr[i];
+  }
+  return (
+    <>
+    <h1>sum:{sum}</h1>
+    </>
+  )
+}
+export default
+/*function Vicky()
+{
+  var arr=[11,22,33,44,55]
+  
+
+  return(
+    <>
+    <ol type="1">
+    {arr.map((item)=>{(item%2===0 && <li>{item}</li>))}
+  </ol>
+  </>
+  )
+}
+export default Vicky
+/*function Vicky ()
+{
+  var sub={
+    rno:1991,
+    sname:"vicky",
+    mark:{tamil:38,english:44,maths:77}
+  }
+  const dis=({rno,sname,mark:{tamil,english,maths}})=>
+  {
+    return[rno,sname,tamil,english,maths]
+  }
+  var [v1,v2,v3,v4,v5]=dis(sub)
+  return (
+    <>
+    <h1>Object destructing</h1>
+    student no:{v1}<br></br>
+    student name:{v2}<br></br>
+    student mark:<br></br>
+            tamil:{v3}<br></br>
+            english:{v4}<br></br>
+            maths:{v5}<br></br>
+    </>
+  )
+}
+export default Vicky
+/*function Vicky()
+{
+  const show=(a,b)=>
+  {
+    var add=a+b;
+    var sub=a-b;
+    var mul=a*b;
+    var div=a/b;
+    return [add,sub,mul,div]
+  }
+  var [a1,a2,a3,a4]=show(11,22)
+  return(
+    <>
+    Addition:{a1}<br></br>
+    substraction:{a2}<br></br>
+    multiplication:{a3}<br></br>
+    division:{a4}
+    </>
+  )
+}
+export default Vicky
+/*function Vicky ()
+{
+  var [v1,,v2,,...v3]=[11,22,33,44,55,66,77,88]
+  return (
+    <>
+    <h1>Array destructing:To grt value from source</h1>
+    value:{v1}
+    <br></br>
+    value 2:{v2}
+    <br></br>
+    value:{v3}
+    </>
+  )
+}
+export default Vicky
+/*function Vicky()
+
+{
+  const show=(a,b,c)=>{
+    if (true)
+    {
+      var k=100;
+      k++;
+    }
+    document.getElementById("r1").innerHTML="hi"+(a+b+c+k)
+  }
+  return(
+<>
+    <button onClick={()=>show(10,10,10)}>click me</button>
+    <div id="r1"></div>
+</>
+  )
+}
+export default Vicky
+/*function Vicky(
+  {
+    const show=()=>{
+      document.getElementById("r1").innerHTML="HI"
+    }
+    function dis()
+    {
+      document.getElementById("r2").innerHTML="hi"
+    }
+    return (
+      <>
+      <h1>without parameter</h1>
+      <button onClick={dis}>click me</button>
+      <button onClick={()=>dis()}>click me</button>
+      <button onClick={show}>click me</button>
+      <div id="r1"></div>
+      <div id="r2"></div>
+      
+      </>
+    )
+  }
+  export default Vicky
+/*function Vicky()
+{
+  var stud=[{rno:1001,name:"vicky",mark:100},
+            {rno:1002,name:"preethi",mark:98},
+            {rno:1003,name:"prabha",mark:38}
+  ]
+  return(
+    <>
+    <h1>student no:{stud[0].rno}</h1>
+    <h1>student name:{stud[0].name}</h1>
+    <h1>student mark:{stud[0].mark}</h1>
+    <h1>student no:{stud[1].rno}</h1>
+    <h1>student name:{stud[1].name}</h1>
+    <h1>student mark:{stud[1].mark}</h1>
+    </>
+  )
+}
+export default Vicky
+/*function Vicky()
+{
+  var stud={
+    rno:1001,
+    sname:"vicky",
+    subject:[77,88,99]
+  }
+  return(
+    <>
+    <h1>student mark:{stud.subject[0]}</h1>
+    <h1>student mark:{stud.subject[1]}</h1>
+    <h1>student mark:{stud.subject[2]}</h1>
+    </>
+  )
+}
+export default Vicky
+/*function Vicky ()
+{
+  var stud={
+    rno:1001,
+    sname:"vicky",
+    subject:{mark1:100,
+    mark2:38,
+    mark3:40}
+  }
+  return(
+    <>
+    <h1>student no:{stud.rno}</h1>
+    <h1>student name:{stud.sname}</h1>
+    <h1>student mark1:{stud.subject["mark1"]}</h1>
+    <h1>student mark2:{stud.subject["mark2"]}</h1>
+    <h1>student mark3:{stud.subject["mark3"]}</h1>
+    </>
+  )
+}
+export default Vicky
+/*function App()
 {
   var rate=5000;
   return(
